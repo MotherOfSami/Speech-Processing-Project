a4eb036a9b3f7c8d3c1d2087b134222f45d1ccbd
rg442 <rgaia412@gmail.com>
2025-02-26 19:41:00 +0200

    fig 4+5+6 + scipy convolve

--- params.py
+++ params.py
@@ -7,7 +7,7 @@ T_60_RANGE = [0.16, 0.32]
 N_FFT_LIST = np.array([2048, 4096, 8192, 16384])  # Num of DFT Points
 D = 0.05  # [m]
 MIC_POSITIONS = [[3 - 3 * (D / 2), 2.5, 2], [3 - (D / 2), 2.5, 2], [3 + (D / 2), 2.5, 2], [3 + 3 * (D / 2), 2.5, 2]]
-REF_MIC_INDEX = 3
+REF_MIC_INDEX = 0
 
 SOURCE_DEGREE = np.pi / 4  # Radians
 SOURCE_RADIUS = 1  # [m]
--- scores_utils.py
+++ scores_utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from pesq import pesq
 from pystoi import stoi
+from prop_algs import reconstruct_full_spectrum
+
 
 def evaluate_pesq_score(fs, ref_mic_signal, deg_signal_complex):
     deg_signal_real = deg_signal_complex.real  # the complex part is an artifact of the processing
@@ -27,3 +29,9 @@ def evaluate_si_sdr_score(ref_mic_signal, deg_signal_complex):
     return score_si_sdr
 
 
+def evaluate_distortion_ratio(F, a_f, w_f_time_freq):
+    a_f_full = reconstruct_full_spectrum(a_f)
+
+    distortion_val = abs(1 - (np.diag(a_f_full.conj() @ w_f_time_freq))) ** 2
+    dr = 10 * np.log10(F / np.sum(distortion_val))
+    return dr
